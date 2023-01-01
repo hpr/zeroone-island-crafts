51ae4f849b31ffa0664ac475c85ffcd8f6e5d7eb
Harry <habs@sdf.org>
2023-01-01 12:26:43 -0500

    add non-food items

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { AgGridReact } from 'ag-grid-react';
+import { items } from './items';
+import { Item } from './types';
+import { ColDef } from 'ag-grid-community';
 
 function App() {
+  const gridRef = React.useRef<AgGridReact>(null);
+  const keys = Object.keys(items) as Item[];
+  const [rowData, setRowData] = React.useState(keys.map((field) => ({
+    item: field,
+    ...items[field],
+    // ...keys.reduce((acc, key) => {
+    //   if (field in items[key]) {
+    //     const val = items[key][field];
+    //     acc[key] = Array.isArray(val) ? val.join(', ') : val;
+    //   }
+    //   return acc;
+    // }, {} as { [k in Item]?: Item | string }),
+  })));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="ag-theme-alpine" style={{ width: '100%', height: '100vh' }}>
+        <AgGridReact
+          enableBrowserTooltips
+          ref={gridRef}
+          rowData={rowData}
+          defaultColDef={{ resizable: true, width: 70, tooltipValueGetter: ({ data, colDef }) => data[(colDef as ColDef).field!] }}
+          columnDefs={[{ field: 'item', pinned: 'left', width: 100 }, ...Object.keys(items).map((field) => ({ field }))]}
+        />
+      </div>
     </div>
   );
 }
