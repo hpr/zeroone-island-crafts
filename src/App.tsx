e9e03c73321624fb245cdbcd795fcfc66718fc3a
Harry <habs@sdf.org>
2023-01-02 09:12:52 -0500

    filter only craftables

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,26 @@ import { ColDef, IHeaderParams } from 'ag-grid-community';
 
 function App() {
   const gridRef = React.useRef<AgGridReact>(null);
-  const keys = Object.keys(items) as Item[];
+  const keys = (Object.keys(items) as Item[]).filter((key) => Object.keys(items[key]).length);
   const [rowData, setRowData] = React.useState(
-    keys.map((field) => ({
-      item: field,
-      ...items[field],
-      // ...keys.reduce((acc, key) => {
-      //   if (field in items[key]) {
-      //     const val = items[key][field];
-      //     acc[key] = Array.isArray(val) ? val.join(', ') : val;
-      //   }
-      //   return acc;
-      // }, {} as { [k in Item]?: Item | string }),
-    }))
+    keys
+      .map((field) => ({
+        item: field,
+        ...items[field],
+        // ...keys.reduce((acc, key) => {
+        //   if (field in items[key]) {
+        //     const val = items[key][field];
+        //     acc[key] = Array.isArray(val) ? val.join(', ') : val;
+        //   }
+        //   return acc;
+        // }, {} as { [k in Item]?: Item | string }),
+      }))
   );
   return (
     <div className="App">
       <button onClick={() => gridRef.current!.api.exportDataAsCsv()}>Export</button> contact (requests, etc): <a href="mailto:habs@sdf.org">habs@sdf.org</a>,
       discord sbah#1649
-      <div className="ag-theme-alpine" style={{ width: '100%', height: '100vh' }}>
+      <div className="ag-theme-alpine" style={{ width: '100%', height: '97vh' }}>
         <AgGridReact
           enableBrowserTooltips
           columnHoverHighlight
@@ -47,10 +48,14 @@ function App() {
               );
             },
             headerComponent: (props: IHeaderParams) =>
-              props.column.getColId() === 'item' ? props.displayName : <img title={props.column.getColId()} height={20} width={20} src={`assets/${props.column.getColId()}.png`} />,
+              props.column.getColId() === 'item' ? (
+                props.displayName
+              ) : (
+                <img title={props.column.getColId()} height={20} width={20} src={`assets/${props.column.getColId()}.png`} />
+              ),
             sortable: true,
           }}
-          columnDefs={[{ field: 'item', pinned: 'left' }, ...Object.keys(items).map((field) => ({ field }))]}
+          columnDefs={[{ field: 'item', pinned: 'left' }, ...keys.map((field) => ({ field }))]}
         />
       </div>
     </div>
